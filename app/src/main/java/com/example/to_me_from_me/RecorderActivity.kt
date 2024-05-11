0f67140d41d5f3d1d8f2bc4600545a3149645cef
ha-es <dmstj00423@naver.com>
2024-05-11 16:37:40 +0900

    [FEAT] 녹음 xml 생성

---
+++ app/src/main/java/com/example/to_me_from_me/RecorderActivity.kt
@@ -0,0 +1,13 @@
+package com.example.to_me_from_me
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecorderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recorder)
+    }
+
+
+}
