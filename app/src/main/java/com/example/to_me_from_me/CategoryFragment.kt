6f54ec51ef9d879a92818a3ea08e87ed00352412
ha-es <dmstj00423@naver.com>
2024-05-15 13:56:41 +0900

    상단바 수정 및 바텀바 연결

--- app/src/main/java/com/example/to_me_from_me/CategoryFragment.kt
+++ app/src/main/java/com/example/to_me_from_me/CategoryFragment.kt
@@ -1,59 +1,27 @@
 package com.example.to_me_from_me
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class CategoryFragment : BottomSheetDialogFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_category, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // 여기에서 버튼들에 대한 클릭 리스너를 설정할 수 있습니다.
+        val nextButton = view.findViewById<Button>(R.id.next_btn)
+        nextButton.setOnClickListener {
+            // 다음 버튼이 클릭되었을 때 수행할 동작을 여기에 작성합니다.
+        }
+
+        // 필요한 경우 다른 버튼들에 대한 클릭 리스너를 설정할 수 있습니다.
+
+        return view
     }
 }
