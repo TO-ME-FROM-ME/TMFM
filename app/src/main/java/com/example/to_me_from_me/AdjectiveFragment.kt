5d62fc00eb25fc37c83281d000a719f8808d9fa0
alskzldud <alskzldud@naver.com>
2024-05-27 02:25:07 +0900

    다음 버튼 연결

--- app/src/main/java/com/example/to_me_from_me/AdjectiveFragment.kt
+++ app/src/main/java/com/example/to_me_from_me/AdjectiveFragment.kt
@@ -10,20 +10,15 @@ import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class EmojiFragment : BottomSheetDialogFragment() {
+class AdjectiveFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_emoji, container, false)
-
+        val view = inflater.inflate(R.layout.fragment_adjective, container, false)
 
 
         val nextButton = view.findViewById<Button>(R.id.next_btn)
         nextButton.setOnClickListener {
-            val nextFragment = AdjectiveFragment()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, nextFragment)
-                .addToBackStack(null)
-                .commit()
+
         }
         return view
     }
