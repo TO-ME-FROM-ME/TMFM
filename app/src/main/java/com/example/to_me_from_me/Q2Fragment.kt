406d871da04acdf1f20869a14433d484fba023be
alskzldud <alskzldud@naver.com>
2024-05-30 00:55:53 +0900

    토스트 메세지, 글자 수 추가

--- app/src/main/java/com/example/to_me_from_me/Q2Fragment.kt
+++ app/src/main/java/com/example/to_me_from_me/Q2Fragment.kt
@@ -2,11 +2,17 @@ package com.example.to_me_from_me
 
 import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -16,22 +22,80 @@ class Q2Fragment : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_q2, container, false)
 
+        val writeEditText = view.findViewById<EditText>(R.id.write_et)
+        val charCountTextView = view.findViewById<TextView>(R.id.char_count_tv)
+        val layout = view.findViewById<LinearLayout>(R.id.custom_toast_container)
+
 
         val nextButton = view.findViewById<Button>(R.id.next_btn)
         nextButton.setOnClickListener {
-            val nextFragment = Q3Fragment()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, nextFragment)
-                .addToBackStack(null)
-                .commit()
+
+            val textLength = writeEditText.text.length
+            val toastLayout = LayoutInflater.from(requireContext()).inflate(R.layout.toast, layout, false)
+            val toastTv = toastLayout.findViewById<TextView>(R.id.toast_tv)
+
+            when {
+                textLength < 50 -> {
+                    showToast(toastLayout,writeEditText)
+                    toastTv.text = "최소 50자 이상 작성해줘!"
+                }
+
+                textLength > 150 -> {
+                    showToast(toastLayout,writeEditText)
+                    toastTv.text = "150자 이하로 작성해줘!"
+                }
+
+                else -> {
+
+                    // 다음 Fragment화면으로 이동
+                    val nextFragment = Q3Fragment()
+                    parentFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, nextFragment)
+                        .addToBackStack(null)
+                        .commit()
+                }
+
+            }
         }
+
+
+
+        // 실시간 글자 수
+        writeEditText.addTextChangedListener(object  : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+            override fun afterTextChanged(s: Editable?) {
+                val charCount = s?.length ?: 0
+                charCountTextView.text = "$charCount"
+            }
+        })
+
 
         return view
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return BottomSheetDialog(requireActivity(), R.style.TransparentBottomSheetDialogTheme)
 
+    private fun showToast(layout: View, writeEditText: EditText) {
+        val toastLayout = LayoutInflater.from(requireContext()).inflate(R.layout.toast, null, false)
+        val toast = Toast(requireContext())
+        val location = IntArray(2)
+        writeEditText.getLocationOnScreen(location)
+        layout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val yOffset = location[1] - writeEditText.height - toastLayout.measuredHeight
+
+
+        toast.setGravity(Gravity.TOP or Gravity.END, location[0], yOffset)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.view = layout
+        toast.show()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return BottomSheetDialog(requireActivity(), R.style.TransparentBottomSheetDialogTheme).apply {
+            setTitle("1. 왜 두려운, 불안한 감정을 느꼈어?")
+        }
     }
 
 }
