5752d7161d7e358f93fbe291cbf2d8f587fd4ed4
alskzldud <alskzldud@naver.com>
2024-05-30 01:43:15 +0900

    뒤로가기
    onBackPressed() 수정 필요

--- app/src/main/java/com/example/to_me_from_me/WriteLetterActivity.kt
+++ app/src/main/java/com/example/to_me_from_me/WriteLetterActivity.kt
@@ -2,6 +2,7 @@ package com.example.to_me_from_me
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
@@ -14,7 +15,10 @@ class WriteLetterActivity : AppCompatActivity() {
         // 초기 프래그먼트 설정
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, SituationFragment()).commit()
 
-
+        val backButton: ImageView = findViewById(R.id.back_iv)
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 
 }
