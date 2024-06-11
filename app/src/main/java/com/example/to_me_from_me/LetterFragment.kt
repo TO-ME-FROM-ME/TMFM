db2fe44059a3a70ab0f878fc36c29a32b05a1095
alskzldud <alskzldud@naver.com>
2024-06-11 21:31:51 +0900

    편지완성 토스트메세지

--- app/src/main/java/com/example/to_me_from_me/LetterFragment.kt
+++ app/src/main/java/com/example/to_me_from_me/LetterFragment.kt
@@ -4,6 +4,8 @@ import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.InputType
 import android.text.SpannableString
@@ -11,6 +13,7 @@ import android.text.Spanned
 import android.text.TextWatcher
 import android.text.style.RelativeSizeSpan
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -19,7 +22,10 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -80,6 +86,7 @@ class LetterFragment : BottomSheetDialogFragment() {
             }
         }
 
+        val layout = view.findViewById<LinearLayout>(R.id.custom_toast_container)
         val charCountTextView = view.findViewById<TextView>(R.id.char_count_tv)
         val charCount = combinedTextValue?.length ?: 0
         charCountTextView.text = "$charCount"
@@ -92,12 +99,29 @@ class LetterFragment : BottomSheetDialogFragment() {
 
         sendBtn.setOnClickListener {
 
-            val nextFragment = RecorderFragment()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, nextFragment)
-                .addToBackStack(null)
-                .commit()
+            val textLength = letterTV.text.length
+            val toastLayout = LayoutInflater.from(requireContext()).inflate(R.layout.toast, layout, false)
+            val toastTv = toastLayout.findViewById<TextView>(R.id.toast_tv)
+
+            when {
+                textLength < 150 -> {
+                    showToast(toastLayout,letterTV,700)
+                    toastTv.text = "최소 150자 이상 작성해줘!"
+                    letterTV.background = ContextCompat.getDrawable(requireContext(), R.drawable.solid_over_txt)
+                }
+
+                else -> {
+
+                    val nextFragment = RecorderFragment()
+                    parentFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, nextFragment)
+                        .addToBackStack(null)
+                        .commit()
+                }
+
+            }
         }
+
 
 // 실시간 글자 수
         letterTV.addTextChangedListener(object  : TextWatcher {
@@ -112,6 +136,32 @@ class LetterFragment : BottomSheetDialogFragment() {
         })
 
         return view
+    }
+
+
+    private fun showToast(layout: View, writeEditText: EditText, duration: Int) {
+        val toast = Toast(requireContext())
+        val location = IntArray(2)
+        writeEditText.getLocationOnScreen(location)
+        layout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val yOffset = location[1] - 20 - layout.measuredHeight
+
+        toast.setGravity(Gravity.TOP or Gravity.END, location[0], yOffset)
+        toast.view = layout
+
+        val handler = Handler(Looper.getMainLooper())
+        val startTime = System.currentTimeMillis()
+
+        handler.post(object : Runnable {
+            override fun run() {
+                if (System.currentTimeMillis() - startTime < duration) {
+                    toast.show()
+                    handler.postDelayed(this, 700)
+                } else {
+                    toast.cancel()
+                }
+            }
+        })
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
