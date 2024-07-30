69ae4f61827eb16374f0c49674d840335e4c3d9d
ha-es <dmstj00423@naver.com>
2024-07-30 12:23:13 +0900

    [FEAT] 회원가입 추가 및 연결

--- app/src/main/java/com/example/to_me_from_me/LoginActivity.kt
+++ app/src/main/java/com/example/to_me_from_me/LoginActivity.kt
@@ -1,11 +1,33 @@
 package com.example.to_me_from_me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.to_me_from_me.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.loginBtn.setOnClickListener {
+            performLogin()
+        }
+
+
+        binding.signupTv.setOnClickListener {
+            startActivity(Intent(this, SignupEmailActivity::class.java))
+            finish() // 현재 화면 종료
+        }
+
+    }
+
+    private fun performLogin() {
+        // 아이디 비번 확인
     }
 }
---
+++ app/src/main/java/com/example/to_me_from_me/SignupEmailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.to_me_from_me
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_email)
+    }
+}
