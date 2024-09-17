7858f94f997e29da3c2eea87d0b308180cdecb78
ha-es <dmstj00423@naver.com>
2024-09-17 23:37:32 +0900

    [REFACTOR] LoginActivity firebase 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,21 @@ dependencies {
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
-    implementation("com.google.firebase:firebase-auth:23.0.0")
+
+
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+
+    // Firebase libraries
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-storage")
+    implementation ("com.google.firebase:firebase-appcheck")
+    implementation("com.google.firebase:firebase-appcheck-playintegrity:18.0.0")
+
+    implementation("com.google.firebase:firebase-appcheck-ktx:18.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -64,14 +78,6 @@ dependencies {
 
     //recycleView
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
-
-    //firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-database-ktx")
-    implementation ("com.google.firebase:firebase-auth-ktx")
-    implementation ("com.google.firebase:firebase-messaging")
-    implementation ("com.google.firebase:firebase-storage")
 
 
     // line chart
--- app/src/main/java/com/example/to_me_from_me/LoginActivity.kt
+++ app/src/main/java/com/example/to_me_from_me/LoginActivity.kt
@@ -3,20 +3,40 @@ package com.example.to_me_from_me
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.to_me_from_me.Signup.SignupEmailActivity
 import com.example.to_me_from_me.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        FirebaseApp.initializeApp(this)
+        val providerFactory = PlayIntegrityAppCheckProviderFactory.getInstance()
+        FirebaseAppCheck.getInstance().installAppCheckProviderFactory(providerFactory)
+
+        auth = Firebase.auth
+
 
         binding.loginBtn.setOnClickListener {
+            val email = binding.loginInputIdEt.text.toString()
+            val password = binding.loginInputPwdEt.text.toString()
+            signIn(email, password)
             performLogin()
         }
 
@@ -26,6 +46,25 @@ class LoginActivity : AppCompatActivity() {
             finish() // 현재 화면 종료
         }
 
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth?.signInWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //로그인에 성공한 경우 메인 화면으로 이동
+                    Toast.makeText(this, "로그인성공" ,Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    finish()
+
+                } else {
+                    //로그인에 실패한 경우 Toast 메시지로 에러를 띄워준다
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+
+                }
+            }
     }
 
     private fun performLogin() {
