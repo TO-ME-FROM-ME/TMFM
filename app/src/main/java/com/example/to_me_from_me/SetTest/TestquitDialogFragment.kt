9b85ea0466a46d9f43d7c5f4e95962bb3b77d31c
alskzldud <alskzldud@naver.com>
2024-11-19 14:46:42 +0900

    테스트 다이얼로그, 레이아웃 수정

--- app/src/main/java/com/example/to_me_from_me/SetTest/TestquitDialogFragment.kt
+++ app/src/main/java/com/example/to_me_from_me/SetTest/TestquitDialogFragment.kt
@@ -1,15 +1,25 @@
 package com.example.to_me_from_me.SetTest
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.DialogFragment
+import com.example.to_me_from_me.CoachMark.CoachMarkActivity
+import com.example.to_me_from_me.MainActivity
 import com.example.to_me_from_me.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class TestquitDialogFragment : DialogFragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,9 +36,25 @@ class TestquitDialogFragment : DialogFragment() {
         }
 
         // 저장 버튼 클릭 리스너 설정
-        val saveButton = view.findViewById<Button>(R.id.save_btn)
-        saveButton.setOnClickListener {
-            dismiss()
+        val quitButton = view.findViewById<Button>(R.id.quit_btn)
+        quitButton.setOnClickListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user != null) {
+                val userRef = FirebaseFirestore.getInstance().collection("users").document(user.uid)
+                userRef.get()
+                    .addOnSuccessListener { document ->
+                        if (document != null && document.contains("totalScore")) {
+                            // totalScore 필드가 존재하는 경우
+                            val intent = Intent(activity, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            // totalScore 필드가 존재하지 않는 경우
+                            val intent = Intent(activity, CoachMarkActivity::class.java)
+                            startActivity(intent)
+                        }
+                        dismiss() // 다이얼로그 닫기
+                    }
+            }
         }
     }
 
